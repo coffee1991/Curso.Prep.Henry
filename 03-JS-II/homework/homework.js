56cf6f21b254e48e6bc41521c26a24a96b18d32b
coffee1991 <jhonalex1007@hotmail.com>
2021-12-10 18:25:57 -0500

    continuación

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -49,6 +49,18 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma === "aleman") {
+    return "Guten Tag!";
+  }
+  else if(idioma === "mandarin") {
+    return "Ni Hao!";
+  }
+  else if(idioma === "ingles") {
+    return "Hello!";
+  }
+  else {
+    return "hola";
+  }
 }
 
 function colors(color) {
